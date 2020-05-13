23c647103d311a1d9ca0087ffda256b48ffb3840
jmalovera10 <jma.lovera10@uniandes.edu.co>
2020-05-13 09:07:23 -0500

    Change in arn reference

--- cloudformation/create_stacks.sh
+++ cloudformation/create_stacks.sh
@@ -2,8 +2,8 @@
 # When the network part is correctly deployed then uncomment the containers deploy and comment 
 # the network deploy.
 
-echo "Creating network infrastructure...."
-./create.sh networkingstack network.yml network-parameters.json
+#echo "Creating network infrastructure...."
+#./create.sh networkingstack network.yml network-parameters.json
 
-#echo "Creating application infrastructure...."
-#./create.sh containerstack container.yml container-parameters.json
+echo "Creating application infrastructure...."
+./create.sh containerstack container.yml container-parameters.json
