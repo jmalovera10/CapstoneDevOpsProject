ee9b267b48b03d5c3b38cd6f773a513622b2af57
jmalovera10 <jma.lovera10@uniandes.edu.co>
2020-05-14 22:29:08 -0500

    Change deploy params

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,19 +1,19 @@
 pipeline {
   agent any
   stages {
-    stage('Lint HTML') {
+    stage('Linting') {
       steps {
         sh 'tidy -q -e *.html'
       }
     }
+    stage('Build image') {
 
-    stage('Upload to AWS') {
-      steps {
-        withAWS(region: 'us-east-1', credentials: 'AWS Jenkins') {
-          s3Upload(bucket: 'ucty-project', file: 'index.html')
-        }
+    }stage('Push image') {
 
-      }
+    }stage('set current kubectl context') {
+
+    }stage('Deploy container') {
+
     }
 
   }
--- cloudformation/create_stacks.sh
+++ cloudformation/create_stacks.sh
@@ -2,8 +2,15 @@
 # When the network part is correctly deployed then uncomment the containers deploy and comment 
 # the network deploy.
 
+# STAGE 1
 #echo "Creating network infrastructure...."
 #./create.sh networkingstack network.yml network-parameters.json
 
+# STAGE 2
+#
+#echo "Creating bastion infrastructure...."
+#./create.sh bastionstack bastion.yml bastion-parameters.json
+
+# STAGE 3
 echo "Creating application infrastructure...."
-./create.sh containerstack container.yml container-parameters.json
+./create.sh kubernetesstack kubernetes.yml kubernetes-parameters.json
