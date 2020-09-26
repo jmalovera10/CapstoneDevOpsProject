7711443e98bb97288ab2e49c4cb44e0450ac44f0
jmalovera10 <jma.lovera10@uniandes.edu.co>
2020-09-26 14:25:33 -0500

    Fix jenkins file

--- cloudformation/create_environment.sh
+++ cloudformation/create_environment.sh
@@ -2,21 +2,19 @@
 aws cloudformation deploy \
     --template-file "./stacks/network.yml" \
     --stack-name "capstone-network" \
-    --tags project=capstone || echo "Network stack already exists"
+    --tags project=capstone
 aws cloudformation wait stack-create-complete --stack-name "capstone-network"
-
-# Deploy EKS Cluster
-aws cloudformation deploy \
-    --template-file "./stacks/eks.yml" \
-    --stack-name "capstone-eks" \
-    --tags project=capstone \
-    --capabilities CAPABILITY_NAMED_IAM || echo "Kubernetes Cluster stack already exists"
-aws cloudformation wait stack-create-complete --stack-name "capstone-eks"
 
 # Deploy Bastion
 aws cloudformation deploy \
     --template-file "./stacks/bastion.yml" \
     --stack-name "capstone-bastion" \
-    --tags project=capstone \
-    --capabilities CAPABILITY_NAMED_IAM || echo "Bastion stack already exists"
+    --tags project=capstone
 aws cloudformation wait stack-create-complete --stack-name "capstone-bastion"
+
+# Deploy EKS Cluster
+aws cloudformation deploy \
+    --template-file "./stacks/eks.yml" \
+    --stack-name "capstone-eks" \
+    --tags project=capstone
+aws cloudformation wait stack-create-complete --stack-name "capstone-eks"
