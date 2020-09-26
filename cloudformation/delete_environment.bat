320cced2a67253b13dd4eb17721e4f19e2f1a4d6
jmalovera10 <jma.lovera10@uniandes.edu.co>
2020-09-26 11:11:03 -0500

    Feedback on cleanup

--- cloudformation/delete_environment.bat
+++ cloudformation/delete_environment.bat
@@ -3,7 +3,12 @@ aws cloudformation delete-stack --stack-name "capstone-eks"
 aws cloudformation delete-stack --stack-name "capstone-bastion"
 
 aws cloudformation wait stack-delete-complete --stack-name "capstone-eks"
+echo "EKS Deleted..."
 aws cloudformation wait stack-delete-complete --stack-name "capstone-bastion"
+echo "Bastion Deleted..."
 
 :: Delete Networking Stack
 aws cloudformation delete-stack --stack-name "capstone-network"
+aws cloudformation wait stack-delete-complete --stack-name "capstone-network"
+echo "Network Deleted..."
+echo "Cleanup complete Deleted..."
